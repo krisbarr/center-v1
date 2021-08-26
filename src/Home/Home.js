46e22e5915ae8bf38a83a3f3aff599f5df87e0df
kris.leanne.barrett@gmail.com <scripts/git.sh>
2021-08-26 13:13:03 -0400

    Complete styling, add logo, add google fonts

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import salutation from '../images/salutation.jpg'
-
+import logo from '../images/centerYogaGradientHorizontal.png'
+import clouds from '../images/clouds.jpeg'
 class Home extends Component {
     render() {
         return (
         <div>
           <div class="flex-w flex-str size1 overlay1">
-            <div class="flex-w flex-col-sb wsize1 bg0 p-l-65 p-t-37 p-b-50 p-r-80 respon1">
+            <div class="flex-w flex-col-sb wsize1 bg0 p-l-65 p-t-37 p-b-50 p-r-80 respon1" style={{backgroundImage: `url(${clouds})`, backgroundPosition: 'center center no-repeat',
+      backgroundSize: 'cover'}}>
               <div class="wrappic1">
               </div>
               <div class="w-full flex-c-m p-t-80 p-b-90">
                 <div class="wsize2">
-                  <h3 class="l1-txt1 p-b-34 respon3">
-                    Center Yoga
-                  </h3>
+                  <div class="l1-txt1 p-b-34 respon3">
+                  <img className="logo" src={logo} alt ="Center Yoga Logo"/>
+                  </div>
 
                   <p class="m2-txt1 p-b-46">
-                    Follow us for updates!
+                    COMING OCTOBER 2021
                   </p>
+                  <p className="m2-txt2 p-b-46"> Subscribe to learn about new student rates and Founding Member packages. </p>
                   <div className="input-group">
                     <span class="wrap-input100 validate-input m-lr-auto-lg" data-validate = "Email is required: ex@abc.xyz">
                       <input class="s2-txt1 placeholder0 input100 trans-04" type="text" name="email" placeholder="Email Address"/>
@@ -27,16 +30,14 @@ class Home extends Component {
                 </div>
               </div>
               <div class="flex-w">
-                <a href="https://www.google.com/" class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
-                  <i class="fa fa-facebook"></i>
+                <a href="https://www.instagram.com/centeryoga.nyc/"class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
+                  <i class="fa fa-instagram"></i>
                 </a>
+                <a href="https://www.instagram.com/centeryoga.nyc/"class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
+                  <i class="fa fa-facebook-square"></i>
+                </a>
+                <p className="m2-txt3 p-b-46"><i class="fa fa-copyright" aria-hidden="true"></i> 2021 Center Yoga | 110 E. 23rd St. NYC, NY 10010 </p>
 
-                <a href="https://www.google.com/" class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
-                  <i class="fa fa-twitter"></i>
-                </a>
-                <a href="https://www.google.com/" class="size3 flex-c-m how-social trans-04 m-r-15 m-b-10">
-                  <i class="fa fa-youtube-play"></i>
-                </a>
               </div>
             </div>
             <div class="wsize1 simpleslide100-parent respon2">
